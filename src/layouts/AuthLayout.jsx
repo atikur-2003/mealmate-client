d96f9a0efa2e6a6412af219014630d0d5c595fd3
Atikur Rahman <atikur947382@gmail.com>
2025-07-09 21:43:57 +0600

    added pricing and footer section

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router';
+import React from "react";
+import { Outlet } from "react-router";
+import lottie from '../assets/lottie files/login.json'
+import Lottie from "lottie-react";
 
 const AuthLayout = () => {
-    return (
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+       <Lottie
+       style={{
+            width: '300px'
+          }}
+          animationData={lottie}
+           loop={true}
+       ></Lottie>
         <div>
-            <Outlet></Outlet>
+          <Outlet></Outlet>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
--- src/pages/authPages/Register.jsx
+++ src/pages/authPages/Register.jsx
@@ -1,25 +1,23 @@
 import React from "react";
+import Lottie from "lottie-react";
 
 const Register = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mr-10">
+        <div className="card-body">
+          <form>
             <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
-          </div>
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+          </form>
         </div>
       </div>
     </div>
